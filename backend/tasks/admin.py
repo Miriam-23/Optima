fce4c8d12898d5aa30b7922688cb274452d13481
Marcos Carcamo <carcamoarroyomarcos@gmail.com>
2026-06-24 13:10:43 -0600

    limpieza de repo y correccion de gitignore

--- backend/tasks/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Estado, Tarea, TareaUsuario, Comentario
-
-# Register your models here.
-admin.site.register(Estado)
-admin.site.register(Tarea)
-admin.site.register(TareaUsuario)
-admin.site.register(Comentario)
--- backend/users/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Rol
-
-# Register your models here.
-admin.site.register(Rol)
-# Nota: No registramos al 'Usuario' porque Django ya lo incluye por defecto.
